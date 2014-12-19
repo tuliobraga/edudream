e9db73f98c91184b62a38ec5d6d0ddc8f2fe0f8f
Túlio Braga <tuliobragam@gmail.com>
2014-12-18 22:33:46 -0500

    facebook login - session start

--- php/module/User/src/User/Controller/LoginController.php
+++ php/module/User/src/User/Controller/LoginController.php
@@ -8,21 +8,27 @@ class LoginController extends AbstractActionController
 {
     public function indexAction()
     {
-        return new ViewModel();
+        $viewModel = new ViewModel();
+
+        session_start();
+        \Facebook\FacebookSession::setDefaultApplication('1410290089262851', '71144b560323842d1a6fffeb9cbec9e7');
+        $redirectUrl = $this->url()->fromRoute('home');
+        $helper = new \Facebook\FacebookRedirectLoginHelper('http://104.236.104.98/'.$redirectUrl.'login/facebook');
+        $loginUrl = $helper->getLoginUrl();
+        
+        $viewModel->setVariable('loginUrl', $loginUrl);
+        return $viewModel;
     }
 
     public function facebookAction() {
         $code = $this->params()->fromQuery('code');
         $state = $this->params()->fromQuery('state');
         $redirectUrl = $this->url()->fromRoute('home');
-        session_start();
-        \Facebook\FacebookSession::setDefaultApplication('1410290089262851', '71144b560323842d1a6fffeb9cbec9e7');
+
         if($code === null) {
+            session_start();
+            \Facebook\FacebookSession::setDefaultApplication('1410290089262851', '71144b560323842d1a6fffeb9cbec9e7');
             $helper = new \Facebook\FacebookRedirectLoginHelper('http://104.236.104.98/'.$redirectUrl.'login/facebook');
-            $loginUrl = $helper->getLoginUrl();
-            $this->redirect()->toUrl($loginUrl);
-        } else {
-            $helper = new \Facebook\FacebookRedirectLoginHelper();
             $session = $helper->getSessionFromRedirect();
             var_dump($session);die;
         }
