219ff746fa2c87ab8d8f3893d240606ed6249b02
Túlio Braga <tuliobragam@gmail.com>
2014-12-18 22:20:29 -0500

    facebook login - session start

--- php/module/User/src/User/Controller/LoginController.php
+++ php/module/User/src/User/Controller/LoginController.php
@@ -15,11 +15,9 @@ class LoginController extends AbstractActionController
         $code = $this->params()->fromQuery('code');
         $state = $this->params()->fromQuery('state');
         $redirectUrl = $this->url()->fromRoute('home');
+        session_start();
         \Facebook\FacebookSession::setDefaultApplication('1410290089262851', '71144b560323842d1a6fffeb9cbec9e7');
-
         if($code === null) {
-            session_start();
-
             $helper = new \Facebook\FacebookRedirectLoginHelper('http://104.236.104.98/'.$redirectUrl.'login/facebook');
             $loginUrl = $helper->getLoginUrl();
             $this->redirect()->toUrl($loginUrl);
