058ceac717c910d2a9a45b43de9612e03a7e7c1e
Túlio Braga <tuliobragam@gmail.com>
2014-12-26 03:08:46 -0500

    fix sign up form

--- php/module/User/src/User/Model/UsersTable.php
+++ php/module/User/src/User/Model/UsersTable.php
@@ -33,10 +33,7 @@ namespace User\Model;
     {
         $rowset = $this->tableGateway->select(array('email' => $email));
         $row = $rowset->current();
-        var_dump($row->password);
-        var_dump($password);
-        var_dump(md5($password));die;
-        if($row->password !== md5($password)) {
+        if($row->password !== $password) {
             $row = false;
         }
         return $row;
